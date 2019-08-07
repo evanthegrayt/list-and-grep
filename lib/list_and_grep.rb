94ecfeb319ca9e52fe643ecc71eba958bd3c1dd1
Evan Gray <evanthegrayt@vivaldi.net>
2019-08-06 21:56:10 -0500

    Add core_ext dir

--- lib/list_and_grep.rb
+++ lib/list_and_grep.rb
@@ -1,7 +1,14 @@
-require_relative 'list_and_grep/file_array'
 require_relative 'list_and_grep/keyword_finder'
 require_relative 'list_and_grep/version'
 
+##
+# Core extension files. Every *.rb file in this directory will be required.
+Dir.glob(File.join(File.dirname(__FILE__), 'core_ext', '*.rb')).each do |file|
+  require file
+end
+
+##
+# Set up module and constants. Identify the config file.
 module ListAndGrep
   CONFIG_FILE = File.join(ENV['HOME'], '.lsgrc')
 end
